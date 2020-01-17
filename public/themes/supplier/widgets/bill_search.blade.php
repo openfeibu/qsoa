616667222fa143874f0958e2290b2e6aef487315
wzj <wzj@e63abde9-1e55-2c42-b17b-c9693ebc8f80>
2020-01-17 03:52:08 +0000

    git-svn-id: https://39.108.144.9:8443/svn/qsoa@26 e63abde9-1e55-2c42-b17b-c9693ebc8f80

--- public/themes/supplier/widgets/bill_search.blade.php
+++ public/themes/supplier/widgets/bill_search.blade.php
@@ -1,17 +1,17 @@
 <div class="layui-row mb10">
     <div class="layui-inline">
-        <select name="supplier_id" class="layui-select search_key">
-            <option value="">{{ trans('supplier.name') }}</option>
-            @foreach($suppliers as $key => $supplier)
-                <option value="{{ $supplier['id'] }}">{{ $supplier['name'] }}</option>
-            @endforeach
-        </select>
-    </div>
-    <div class="layui-inline">
         <select name="airport_id" class="layui-select search_key">
             <option value="">{{ trans('airport.name') }}</option>
             @foreach($airports as $key => $airport)
                 <option value="{{ $airport['id'] }}">{{ $airport['name'] }}</option>
+            @endforeach
+        </select>
+    </div>
+    <div class="layui-inline">
+        <select name="supplier_id" class="layui-select search_key">
+            <option value="">{{ trans('supplier.name') }}</option>
+            @foreach($suppliers as $key => $supplier)
+                <option value="{{ $supplier['id'] }}">{{ $supplier['name'] }}</option>
             @endforeach
         </select>
     </div>
