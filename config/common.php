f3fabb8e7a1efc2c0b143c5e20df7abda41b5949
wzj <wzj@e63abde9-1e55-2c42-b17b-c9693ebc8f80>
2020-03-20 06:50:11 +0000

    git-svn-id: https://39.108.144.9:8443/svn/qsoa@68 e63abde9-1e55-2c42-b17b-c9693ebc8f80

--- config/common.php
+++ config/common.php
@@ -8,7 +8,7 @@ return [
         'xlsx','xls','csv','txt','doc','dos','pdf','docx','rar','zip'
     ],
 	'img_size' => 1024 * 1024 * 10,
-    'file_size' => 1024 * 1024 * 10,
+    'file_size' => 1024 * 1024 * 50,
     'default_avatar' => '/system/avatar.jpeg',
     'auth_file' => '/system/auth_file.jpeg',
     'qq_map_key' => env('QQ_MAP_WEB_KEY'),
